d127be336e884dee0451fa8106b16a198db3a2f2
Steve Majewski <sdm7g@virginia.edu>
2019-04-12 14:43:11 -0400

    use global python log

--- oaiharvest/client.py
+++ oaiharvest/client.py
@@ -1,10 +1,12 @@
 from oaipmh.client import *
 import logging
-
+from lxml import etree
+print( __file__ )
+etree.use_global_python_log(etree.PyErrorLog())
 
 class RecoveringClient(Client):
 # oaipmh Client with option to recover from bad XML in metadata payload
-    logger = logging.getLogger( 'oaiharvest.harvest')
+    logger = logging.getLogger( 'oaiharvest.harvest' ).getChild( 'client' )
     def parse(self, xml):
         """Parse the XML to a lxml tree WITH parser option: recover=True.
         """
@@ -25,14 +27,12 @@ class RecoveringClient(Client):
             return etree.XML( xml )
         except etree.XMLSyntaxError as perror:
             self.logger.error(perror)
-            print( perror.error_log )
             try:
                 tree = etree.XML(xml, etree.XMLParser(recover=True)) # sdm7g: attempt to RECOVER
                 id = tree.xpath( '/oai:OAI-PMH/oai:ListRecords/oai:record/oai:header/oai:identifier', namespaces=self.getNamespaces() )
-                self.logger.warning( "Recoverable parse error on: {0}.oai_ead.xml".format(id[0].text) )
+                self.logger.warning( "Recoverable parse error on: {0}".format(id[0].text) )
             except Exception as perror2:
                 self.logger.error(perror2)
-            #         
             return tree
 
 
